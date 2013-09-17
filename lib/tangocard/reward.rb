ee95fee5d3f1aedd9f24eabecc644225e1331160
Raphael Crawford-Marks <raphael.crawfordmarks@gmail.com>
2013-09-17 18:01:52 -0400

    Fix rdoc comments for Tangocard::Reward

--- lib/tangocard/reward.rb
+++ lib/tangocard/reward.rb
@@ -11,29 +11,29 @@ class Tangocard::Reward
     @max_price = params['max_price'].to_i
   end
 
+  # Is this a variable-priced reward?
+  #
+  # Example:
+  #   >> reward.variable_price?
+  #   => true # reward is variable-priced
+  #
+  # Arguments:
+  #   none
   def variable_price?
-    # Is this a variable-priced reward?
-    #
-    # Example:
-    #   >> reward.variable_price?
-    #   => true # reward is variable-priced
-    #
-    # Arguments:
-    #   none
     self.unit_price == -1
   end
 
+  # Is this reward purchasable given a certain number of cents available to purchase it?
+  # True if reward is available and user has enough cents
+  # False if reward is unavailable OR user doesn't have enough cents
+  #
+  # Example:
+  #   >> reward.purchasable?(500)
+  #   => true # reward is available and costs <= 500 cents
+  #
+  # Arguments:
+  #   balance_in_cents: (Integer)
   def purchasable?(balance_in_cents)
-    # Is this reward purchasable given a certain number of cents available to purchase it?
-    # True if reward is available and user has enough cents
-    # False if reward is unavailable OR user doesn't have enough cents
-    #
-    # Example:
-    #   >> reward.purchasable?(500)
-    #   => true # reward is available and costs <= 500 cents
-    #
-    # Arguments:
-    #   balance_in_cents: (Integer)
     return false unless available
 
     if variable_price?
