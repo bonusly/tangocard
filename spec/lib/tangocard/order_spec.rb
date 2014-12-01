7d8d76b86dbd8144c49ad2e37e81c2a2d73ce76c
Raphael Crawford-Marks <raphael.crawfordmarks@gmail.com>
2014-12-01 13:55:26 -0500

    double to single quote

--- spec/lib/tangocard/order_spec.rb
+++ spec/lib/tangocard/order_spec.rb
@@ -3,47 +3,47 @@ require 'spec_helper'
 describe Tangocard::Order do
   include TangocardHelpers
 
-  describe "class methods" do
-    describe "self.all" do
+  describe 'class methods' do
+    describe 'self.all' do
       let(:params) { Object.new }
       let(:success_response) { sample_order_index_response(true) }
       let(:fail_response) { sample_order_index_response(false) }
 
-      it "should return an array of Tangocard::Order objects if successful" do
+      it 'should return an array of Tangocard::Order objects if successful' do
         mock(Tangocard::Raas).orders_index(params) { success_response }
         mock(Tangocard::Order).new(success_response.parsed_response['orders'].first) { true }
         Tangocard::Order.all(params).should == [true]
       end
 
-      it "should return an empty array if unsuccessful" do
+      it 'should return an empty array if unsuccessful' do
         mock(Tangocard::Raas).orders_index(params) { fail_response }
         Tangocard::Order.all(params).should == []
       end
     end
 
-    describe "self.find" do
+    describe 'self.find' do
       let(:order_id) { Object.new }
       let(:success_response) { sample_find_order_response(true) }
       let(:fail_response) { sample_find_order_response(false) }
 
-      it "should return a Tangocard::Order object if successful" do
+      it 'should return a Tangocard::Order object if successful' do
         mock(Tangocard::Raas).show_order({'order_id' => order_id}) { success_response }
         mock(Tangocard::Order).new(success_response.parsed_response['order']) { true }
         Tangocard::Order.find(order_id).should be_true
       end
 
-      it "should throw a Tangocard::OrderNotFoundException if failed" do
+      it 'should throw a Tangocard::OrderNotFoundException if failed' do
         mock(Tangocard::Raas).show_order({'order_id' => order_id}) { fail_response }
         lambda{ Tangocard::Order.find(order_id) }.should raise_error(Tangocard::OrderNotFoundException)
       end
     end
 
-    describe "self.create" do
+    describe 'self.create' do
       let(:params) { Object.new }
       let(:success_response) { sample_create_order_response(true) }
       let(:fail_response) { sample_create_order_response(false) }
 
-      it "should return at Tangocard::Order object if successful" do
+      it 'should return at Tangocard::Order object if successful' do
         mock(Tangocard::Raas).create_order(params) { success_response }
         mock(Tangocard::Order).new(success_response.parsed_response['order']) { true }
         Tangocard::Order.create(params).should be_true
@@ -56,11 +56,11 @@ describe Tangocard::Order do
     end
   end
 
-  describe "instance methods" do
-    describe "initialize" do
+  describe 'instance methods' do
+    describe 'initialize' do
       let(:params) { Object.new }
 
-      it "should set the attributes on the object" do
+      it 'should set the attributes on the object' do
         mock(params).[]('order_id') { 'order_id'}
         mock(params).[]('account_identifier') { 'account_identifier'}
         mock(params).[]('customer') { 'customer'}
@@ -77,12 +77,12 @@ describe Tangocard::Order do
       end
     end
 
-    describe "reward" do
-      pending "TODO"
+    describe 'reward' do
+      pending 'TODO'
     end
 
-    describe "identifier" do
-      pending "TODO"
+    describe 'identifier' do
+      pending 'TODO'
     end
   end
 end
